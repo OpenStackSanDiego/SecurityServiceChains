66a75f146958f0e83a5409cd6857f012b6a0726c
johnstudarus <john@jhlconsulting.com>
2017-09-01 15:24:52 -0700

    Update add_lab_environments.sh

--- add_lab_environments.sh
+++ add_lab_environments.sh
@@ -39,6 +39,8 @@ cat >> $USER_HOME/.bashrc << EOF
 . ~/keystonerc
 EOF
 
+# copy over the answers
+wget https://raw.githubusercontent.com/OpenStackSanDiego/SecurityServiceChains/master/Lab1-Answers.sh -O ~user02/Lab1-Answers.sh
 
 
 PROJECT_ID=`openstack project create $PROJECT -f value -c id`
