4379d3a8241bcc4cbf6443860769f80aaee74729
johnstudarus <john@jhlconsulting.com>
2017-09-04 15:54:15 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -91,7 +91,12 @@ wget -q -O - $IMG_URL | \
 glance  --os-image-api-version 2 image-create --protected True --name $IMG_NAME \
         --visibility public --disk-format raw --container-format bare --property os_distro=$OS_DISTRO --progress
 
-	
+IMG_URL=https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2
+IMG_NAME=CentOS-7
+OS_DISTRO=centos
+wget -q -O - $IMG_URL | \
+glance  --os-image-api-version 2 image-create --protected True --name $IMG_NAME \
+        --visibility public --disk-format raw --container-format bare --property os_distro=$OS_DISTRO --progress	
 
 ## end of cloud customization
 	
