a05274c4577f87111927d833ecda56c5b0fdba99
johnstudarus <john@jhlconsulting.com>
2017-09-15 07:47:46 -0700

    Update Lab1-Answers.sh

--- Lab1-Answers.sh
+++ Lab1-Answers.sh
@@ -2,40 +2,88 @@
 # life is more fun when you do it yourself...
 # but sometimes you do get stuck and we understand  :)
 
+# Virtual Machine Credentials
+openstack keypair create --public-key ~/.ssh/id_rsa.pub default
+
+# Networking Setup
 openstack security group rule create --dst-port 80 --protocol tcp --ingress default
 openstack security group rule create --dst-port 22 --protocol tcp --ingress default
 
-INTERNAL_NETWORK_ID=`openstack network show internal -c id -f value`
+# Network Ports
+for port in port-admin1 port-ingress1 port-egress1 port-webclient port-webserver
+do
+    openstack port create --network internal "${port}"
+done
 
-openstack server create --image CirrosWeb --flavor m1.tiny --nic net-id=$INTERNAL_NETWORK_ID WebServer -c id -f value
-FLOATING_IP=`openstack floating ip create public -c floating_ip_address -f value`
-openstack server add floating ip WebServer $FLOATING_IP
+# Save IP Address Assignments
+WEBCLIENT_IP=$(openstack port show port-webclient -f value -c fixed_ips | \
+	grep "ip_address='[0-9]*\." | cut -d"'" -f2)
+echo WEBCLIENT_IP=$WEBCLIENT_IP
 
-openstack server create --image CirrosWeb --flavor m1.tiny --nic net-id=$INTERNAL_NETWORK_ID WebClient -c id -f value
-FLOATING_IP=`openstack floating ip create public -c floating_ip_address -f value`
-openstack server add floating ip WebClient $FLOATING_IP
+WEBSERVER_IP=$(openstack port show port-webserver -f value -c fixed_ips | \
+	grep "ip_address='[0-9]*\." | cut -d"'" -f2)
+echo WEBSERVER_IP=$WEBSERVER_IP
 
-INGRESS_PORT_ID=`openstack port create --network internal ingress-01 -c id -f value`
-EGRESS_PORT_ID=`openstack port create --network internal egress-01 -c id -f value`
-
-NETMON_ID=`openstack server create --image NetMon --flavor m1.small \
-        --nic   net-id=$INTERNAL_NETWORK_ID \
-        --nic   port-id=$INGRESS_PORT_ID \
-        --nic   port-id=$EGRESS_PORT_ID \
-        NetMon -c id -f value`
-FLOATING_IP=`openstack floating ip create public -c floating_ip_address -f value`
-openstack server add floating ip NetMon $FLOATING_IP
+NETMON1_ADMIN_IP=$(openstack port show port-admin1 -f value -c fixed_ips | \
+	grep "ip_address='[0-9]*\." | cut -d"'" -f2)
+echo NETMON1_ADMIN_IP=$NETMON1_ADMIN_IP
 
 
-WEBCLIENT_ID=`openstack port list --server WebClient -c ID -f value`
-neutron flow-classifier-create   --description "HTTP traffic from WebClient" \
-                                 --logical-source-port $WEBCLIENT_ID \
-                                 --ethertype IPv4 \
-                                 --protocol tcp \
-                                 --destination-port 80:80 FC1
-neutron port-pair-create --description "NetMon" \
-                         --ingress ingress-01 \
-                         --egress egress-01 PP1
-neutron port-pair-group-create --port-pair PP1 PPG1
-neutron port-chain-create --port-pair-group PPG1 --flow-classifier FC1 PC1
+
+
+# Instances
+openstack server create \
+	--image NetMon \
+	--flavor m1.small \
+	--nic port-id=port-admin1 \
+	--nic port-id=port-ingress1 \
+	--nic port-id=port-egress1 \
+	--key-name default \
+	netmon1
+        
+ openstack server create \
+	--image cirros \
+	--flavor m1.tiny \
+	--nic port-id=port-webclient \
+	--key-name default \
+	webclient
+        
+ openstack server create \
+	--image cirros \
+	--flavor m1.tiny \
+        --nic port-id=port-webserver \
+	--key-name default \
+	webserver
+        
+        
+ # Startup the Web Server
+ssh cirros@${WEBSERVER_IP} \
+	'while true; do echo -e "HTTP/1.0 200 OK\r\n\r\nWelcome to $(hostname)" | sudo nc -l -p 80 ; done&'
+        
+        
+# Test Web Server
+ssh cirros@${WEBCLIENT_IP} curl -s ${WEBSERVER_IP}
+
+# IP Forwarding and Routing Setup
+ssh -T centos@${NETMON1_ADMIN_IP} <<EOF
+sudo ip route add $WEBCLIENT_IP dev eth1
+sudo ip route add $WEBSERVER_IP dev eth2
+sudo /sbin/sysctl -w net.ipv4.ip_forward=1
+EOF
+
+
+# Service Chaining
+openstack sfc flow classifier create \
+    --ethertype IPv4 \
+    --source-ip-prefix ${WEBCLIENT_IP}/32 \
+    --destination-ip-prefix ${WEBSERVER_IP}/32 \
+    --protocol tcp \
+    --destination-port 80:80 \
+    --logical-source-port port-webclient \
+    FC-WebServer-HTTP
+
+
+openstack sfc port pair create --ingress=port-ingress1 --egress=port-egress1 Netmon1-PortPair
+openstack sfc port pair group create --port-pair Netmon1-PortPair Netmon-PairGroup
+openstack sfc port chain create --port-pair-group Netmon-PairGroup --flow-classifier FC-WebServer-HTTP PC1
 
